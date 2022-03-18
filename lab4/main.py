edcfd83d28e80f97b0bb4988849a9c1f1c03675d
hanifaudah <hanifarkanaudah@gmail.com>
2022-03-18 15:57:10 +0700

    add root path when starting app

--- lab4/main.py
+++ lab4/main.py
@@ -1,14 +1,17 @@
 from fastapi import Depends, FastAPI, HTTPException, UploadFile
-
+from os import getenv
+from dotenv import load_dotenv
 # db
 from sqlalchemy.orm import Session
 
 from db import crud, models, schemas
 from db.database import SessionLocal, engine
 
+load_dotenv()
+
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(root_path=getenv("ROOT_PATH"))
 
 # setup db
 def get_db():
